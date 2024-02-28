141213b0bf9196affa75d84c6965fd6dd62766cb
Tristan Marzec <tristan.marzec@gmx.de>
2024-02-28 21:49:34 +0100

    add basic file generator logic

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -1,1 +1,35 @@
 package generator
+
+import (
+	"fmt"
+	"os"
+)
+
+func GenerateYAMLFile(object interface{}, fileName string) {
+
+	yamlData, err := yaml.Marshal(&object)
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = os.WriteFile(fileName, yamlData, 0644)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func UpdateYAMLFile(object interface{}, fileName string) {
+
+}
+
+func DeleteYAMLFile(fileName string) {
+
+	err := os.Remove(fileName)
+
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("File successfully deleted...")
+}
